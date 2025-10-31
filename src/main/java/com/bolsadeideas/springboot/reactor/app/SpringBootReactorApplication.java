64a1d13f38174b474ad23ed201271cd0857f1fea
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-10-30 20:20:43 -0500

    Feat(ReactorAPI): infinite interval, latch and retry

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.reactor.app;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +29,24 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ejemploDelayElements();
+		ejemploIntervalInfinito();
+	}
+
+	public void ejemploIntervalInfinito() throws Exception {
+		CountDownLatch latch = new CountDownLatch(1);
+
+		Flux.interval(Duration.ofSeconds(1))
+				.doOnTerminate(() -> latch.countDown())
+				.flatMap(i -> {
+					if (i >= 5)
+						return Flux.error(new InterruptedException("Solo hasta 5."));
+					return Flux.just(i);
+				})
+				.map(i -> "Hola " + i)
+				.retry(2)
+				.subscribe(tick -> log.info(tick.toString()), e -> log.error(e.getMessage()));
+
+		latch.await();
 	}
 
 	public void ejemploDelayElements() throws Exception {
